c21118e143f573bcfeead0e5947e675d29133282
thevigroup <thevi5179@gmail.com>
2025-04-17 16:22:30 +0800

    Update script.js

--- Media/ad/script.js
+++ Media/ad/script.js
@@ -1,7 +1,7 @@
 const proposalContainer = document.getElementById("proposal");
 
 async function fetchMarkdown() {
-  const url = 'https://raw.githubusercontent.com/barchart/marketdata-api-js/refs/heads/master/README.md';
+  const url = 'https://raw.githubusercontent.com/thevigroup/thevigroup.github.io/refs/heads/main/Media/ad/ad.md';
 
   try {
     const response = await fetch(url);

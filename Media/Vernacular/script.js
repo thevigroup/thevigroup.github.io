73f66678ce0cbfad5f61756a754ee664eabca551
thevigroup <thevi5179@gmail.com>
2025-04-17 18:06:55 +0800

    Update script.js

--- Media/Vernacular/script.js
+++ Media/Vernacular/script.js
@@ -1,7 +1,7 @@
 const proposalContainer = document.getElementById("proposal");
 
 async function fetchMarkdown() {
-  const url = 'https://raw.githubusercontent.com/thevigroup/thevigroup.github.io/refs/heads/main/Media/Vernacular/Vernacular.md';
+  const url = 'https://raw.githubusercontent.com/thevigroup/thevigroup.github.io/refs/heads/main/Media/Vernacular/Vernacular.md%20';
 
   try {
     const response = await fetch(url);
